1276d84e705b3f5ad28ece4f61976ba466158ac3
kiparisik <kanadakiparis@gmail.com>
2026-05-27 21:30:02 +0500

    First move rename form to DodoMainForm

--- DodoMainForm.cs
+++ DodoMainForm.cs
@@ -1,8 +1,8 @@
 namespace dodoEF
 {
-    public partial class Form1 : Form
+    public partial class DodoMainForm : Form
     {
-        public Form1()
+        public DodoMainForm()
         {
             InitializeComponent();
         }
